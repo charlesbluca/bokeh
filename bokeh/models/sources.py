c27a6f99b1752f5db9706978cf61b66db7113b31
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-04-20 08:15:00 -0700

    Add sources implementation of multi_stream

--- bokeh/models/sources.py
+++ bokeh/models/sources.py
@@ -376,6 +376,153 @@ class ColumnDataSource(ColumnarDataSource):
         except (ValueError, KeyError):
             warnings.warn("Unable to find column '%s' in data source" % name)
 
+    def multi_stream(self, new_data, rollover=None):
+        ''' Efficiently update data source columns with new append-only data.
+
+        In cases where it is necessary to update data columns in, this method
+        can efficiently send only the new data, instead of requiring the
+        entire data set to be re-sent.
+
+        Args:
+            new_data (dict[str, seq]) : a mapping of column names to sequences of
+                new data to append to each column.
+
+                All columns of the data source must be present in ``new_data``,
+                with identical-length append data.
+
+            rollover (int, optional) : A maximum column size, above which data
+                from the start of the column begins to be discarded. If None,
+                then columns will continue to grow unbounded (default: None)
+
+        Returns:
+            None
+
+        Raises:
+            ValueError
+
+        Example:
+
+        .. code-block:: python
+
+            source = ColumnDataSource(data=dict(foo=[], bar=[]))
+
+            # has new, identical-length updates for all columns in source
+            new_data = {
+                'foo' : [10, 20],
+                'bar' : [100, 200],
+            }
+
+            source.stream(new_data)
+
+        '''
+        # calls internal implementation
+        self._multi_stream(new_data, rollover)
+
+    def _multi_stream(self, new_data, rollover=None, setter=None):
+        ''' Internal implementation to efficiently update data source columns
+        with new append-only data. The internal implementation adds the setter
+        attribute.  [https://github.com/bokeh/bokeh/issues/6577]
+
+        In cases where it is necessary to update data columns in, this method
+        can efficiently send only the new data, instead of requiring the
+        entire data set to be re-sent.
+
+        Args:
+            new_data (dict[str, seq] or DataFrame or Series) : a mapping of
+                column names to sequences of new data to append to each column,
+                a pandas DataFrame, or a pandas Series in case of a single row -
+                in this case the Series index is used as column names
+
+                All columns of the data source must be present in ``new_data``,
+                with identical-length append data.
+
+            rollover (int, optional) : A maximum column size, above which data
+                from the start of the column begins to be discarded. If None,
+                then columns will continue to grow unbounded (default: None)
+            setter (ClientSession or ServerSession or None, optional) :
+                This is used to prevent "boomerang" updates to Bokeh apps.
+                (default: None)
+                In the context of a Bokeh server application, incoming updates
+                to properties will be annotated with the session that is
+                doing the updating. This value is propagated through any
+                subsequent change notifications that the update triggers.
+                The session can compare the event setter to itself, and
+                suppress any updates that originate from itself.
+        Returns:
+            None
+
+        Raises:
+            ValueError
+
+        Example:
+
+        .. code-block:: python
+
+            source = ColumnDataSource(data=dict(foo=[], bar=[]))
+
+            # has new, identical-length updates for all columns in source
+            new_data = {
+                'foo' : [10, 20],
+                'bar' : [100, 200],
+            }
+
+            source.stream(new_data)
+
+        '''
+        # needs_length_check = True
+
+        if pd and isinstance(new_data, pd.Series):
+            new_data = new_data.to_frame().T
+
+        if pd and isinstance(new_data, pd.DataFrame):
+            # needs_length_check = False # DataFrame lengths equal by definition
+            _df = new_data
+            newkeys = set(_df.columns)
+            index_name = ColumnDataSource._df_index_name(_df)
+            newkeys.add(index_name)
+            new_data = dict(_df.items())
+            new_data[index_name] = _df.index.values
+        else:
+            newkeys = set(new_data.keys())
+
+        oldkeys = set(self.data.keys())
+
+        if newkeys != oldkeys:
+            extra = newkeys - oldkeys
+            if extra:
+                raise ValueError("Must stream updates to existing columns (extra: %s)" % ", ".join(sorted(extra)))
+
+        import numpy as np
+        # if needs_length_check:
+        lengths = set()
+        arr_types = (np.ndarray, pd.Series) if pd else np.ndarray
+        for k, x in new_data.items():
+            if isinstance(x, arr_types):
+                if len(x.shape) != 1:
+                    raise ValueError("stream(...) only supports 1d sequences, got ndarray with size %r" % (x.shape,))
+                lengths.add(x.shape[0])
+            else:
+                lengths.add(len(x))
+
+        if len(lengths) > 1:
+            raise ValueError("All streaming column updates must be the same length")
+
+        # slightly awkward that we have to call convert_datetime_array here ourselves
+        # but the downstream code expects things to already be ms-since-epoch
+        for key, values in new_data.items():
+            if pd and isinstance(values, (pd.Series, pd.Index)):
+                values = values.values
+            old_values = self.data[key]
+            # Apply the transformation if the new data contains datetimes
+            # but the current data has already been transformed
+            if (isinstance(values, np.ndarray) and values.dtype.kind.lower() == 'm' and
+                isinstance(old_values, np.ndarray) and old_values.dtype.kind.lower() != 'm'):
+                new_data[key] = convert_datetime_array(values)
+            else:
+                new_data[key] = values
+
+        self.data._stream(self.document, self, new_data, rollover, setter)
+
     def stream(self, new_data, rollover=None):
         ''' Efficiently update data source columns with new append-only data.
 
